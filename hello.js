2689eef9dc86f687a1518df5aff998b433decc72
kostas <fellowhelp@proton.me>
2024-01-29 11:36:33 +0200

    unused variable readFile

--- hello.js
+++ hello.js
@@ -1,4 +1,7 @@
 //run the above code to see what is happening
+process.on('start', function(){
+    console.log('blah ??')
+});
 process.on('exit', function(){
     console.log('blah blah')
 });
@@ -10,7 +13,7 @@ console. log( 'yum ')
 })
 eventEmitter.emit('lunch');
 
-const { readFile, readFileSync }=require( 'fs');
+const { readFileSync }=require( 'fs');
 const txt= readFileSync('./hello.txt' ,'utf8');
 console. log(txt);
 console.log('do this ASAP')
